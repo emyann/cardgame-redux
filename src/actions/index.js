71d3bf8b34d697d47f9bbf2aedc19a36a3654ed3
Yann Renaudin <renaudin.yann@gmail.com>
2017-10-16 02:10:28 -0400

    feat(Redux): Add redux thunk to handle middleware when dealOneCard

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,9 @@
-export const addPlayer = name => ({ type: 'ADD_PLAYER', name });
+export const addPlayer = userName => ({ type: 'ADD_PLAYER', userName });
 export const shuffle = () => ({ type: 'DECK_SHUFFLE' });
-export const dealOneCard = ({ id: userId }) => ({ type: 'DEAL_ONE_CARD', userId });
+export const dealOneCard = userName => (dispatch, getState) => {
+    const card = getState().deck.remainingCards[0]; // pick up the first card among the remaining one from the deck
+    if(!card){
+        return; 
+    }
+    return dispatch({ type: 'DEAL_ONE_CARD', userName , cardId: card.id}); // dispatch the choosen one to reducers
+}
