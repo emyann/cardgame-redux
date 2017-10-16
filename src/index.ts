7fe9c7dc48cb78996da5846fd204777eb6a779dd
Yann Renaudin <renaudin.yann@gmail.com>
2017-10-16 00:32:03 -0400

    feat(Redux): Basic simulation of adding players and shuffling cards

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-export default class Main {
-    constructor() {
-        console.log('Typescript Webpack starter launched');
-    }
-}
+import { createStore, applyMiddleware } from 'redux';
+import { rootReducer } from './reducers';
+import { addPlayer, shuffle, dealOneCard } from "./actions";
+import * as _ from 'lodash';
 
-let start = new Main();
+const store = createStore(rootReducer);
+store.subscribe(() => {
+    console.log('debug:: state', JSON.stringify(store.getState(), null, 4));
+});
+store.dispatch(addPlayer('yrnd1'));
+store.dispatch(addPlayer('player2'));
+store.dispatch(shuffle());
+// store.dispatch(shuffle());
+// store.dispatch(dealOneCard({ id: '1'}));
+// store.dispatch(shuffle());
+// store.dispatch(shuffle());
+// store.dispatch(dealOneCard({ id: '1'}));
