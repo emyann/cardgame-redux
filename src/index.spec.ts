a52bd90f31f40761ce2d82c460af12819f48abe1
Yann Renaudin <renaudin.yann@gmail.com>
2017-10-16 05:12:02 -0400

    chore: cleanup index.spec.ts

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,12 +1,4 @@
 describe('Cardgame - Redux', () => {
-    it('should throw if player is added without a name', () => {
-        expect(true).toBe(true);
-    });
-
-    it('should throw if a player with an existing name is added', () => {
-        expect(true).toBe(true);
-    });
-
     it('should shuffle the deck after shuffle action', () => {
         expect(true).toBe(true);
     });
