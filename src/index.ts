2f1ac320dfc5ae46ef0968785703384bdc8fce6e
Yann Renaudin <renaudin.yann@gmail.com>
2017-10-16 02:12:20 -0400

    feat(Redux): first stable version and simulation ok

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,30 @@
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { rootReducer } from './reducers';
 import { addPlayer, shuffle, dealOneCard } from "./actions";
 import * as _ from 'lodash';
 
-const store = createStore(rootReducer);
+const middleware = [thunk];
+const store = createStore(rootReducer, applyMiddleware(...middleware));
 store.subscribe(() => {
     console.log('debug:: state', JSON.stringify(store.getState(), null, 4));
 });
 store.dispatch(addPlayer('yrnd1'));
 store.dispatch(addPlayer('player2'));
+
 store.dispatch(shuffle());
-// store.dispatch(shuffle());
-// store.dispatch(dealOneCard({ id: '1'}));
-// store.dispatch(shuffle());
-// store.dispatch(shuffle());
-// store.dispatch(dealOneCard({ id: '1'}));
+store.dispatch(dealOneCard('yrnd1'));
+store.dispatch(dealOneCard('player2'));
+
+store.dispatch(shuffle());
+store.dispatch(dealOneCard('yrnd1'));
+store.dispatch(dealOneCard('player2'));
+
+store.dispatch(shuffle());
+store.dispatch(dealOneCard('yrnd1'));
+store.dispatch(dealOneCard('player2'));
+
+
+store.dispatch(dealOneCard('yrnd1'));
+store.dispatch(dealOneCard('yrnd1'));
+store.dispatch(dealOneCard('yrnd1'));
